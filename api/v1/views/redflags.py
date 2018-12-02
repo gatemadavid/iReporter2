bb6de40cda371499f3a2503162ca3f41d431d339
Gatema David <dshefard@gmail.com>
2018-12-02 20:58:23 +0300

    add create red flag endpoint

---
+++ api/v1/views/redflags.py
@@ -0,0 +1,27 @@
+from flask_restful import Resource
+from flask import jsonify, make_response, request
+from ..models.Redflags import RedFlagsModel, SingleFlagModel
+
+
+class RedFlags(Resource, RedFlagsModel):
+    def __init__(self):
+        self.db = RedFlagsModel()
+
+    def post(self):
+        data = request.get_json()
+        title = data['title']
+        description = data['description']
+        location = data['location']
+        incident_type = data['type']
+        resp = self.db.save(title, description, location, incident_type)
+        return make_response(jsonify({
+            "message": "Red Flag Created",
+            "Red Flags": resp
+        }), 201)
+
+    def get(self):
+        resp = self.db.get_flags()
+        return make_response(jsonify({
+            "Message": "success",
+            "Red Flags": resp
+        }), 200)
