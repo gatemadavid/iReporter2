8b1fcee51282dadb1fb5bd92f930464f73a40aa4
Gatema David <dshefard@gmail.com>
2018-12-05 20:26:28 +0300

    add get one redflag

--- app/api/v2/models/Incidents.py
+++ app/api/v2/models/Incidents.py
@@ -17,7 +17,7 @@ class IncidentsModel():
 
         query = """INSERT INTO incidents (title, incident, location, status, description, createdBy) VALUES
             (%(title)s, %(incident)s, %(location)s, %(status)s, %(description)s, %(createdBy)s)"""
-       
+
         curr = self.db.cursor()
         curr.execute(query, payload)
         self.db.commit()
@@ -44,3 +44,47 @@ class IncidentsModel():
             )
             resp.append(res)
         return resp
+
+
+class IncidentModel():
+    def __init__(self):
+        self.db = init_db()
+
+    def getIncident(self, id):
+        dbconn = self.db
+        curr = dbconn.cursor()
+        curr.execute(
+            """SELECT id, title, incident, location, status, description, createdBy FROM incidents where id=%s; """, [id])
+        data = curr.fetchone()
+        # resp = []
+        # for i, incident in enumerate(data):
+        #     id, title, incident, location, status, description, createdBy = incident
+        #     res = dict(
+        #         id=int(id),
+        #         title=title,
+        #         incident=incident,
+        #         location=location,
+        #         status=status,
+        #         description=description,
+        #         createdBy=createdBy
+
+        #     )
+        #     resp.append(res)
+        return data
+
+    def deleteIncident(self, id):
+        dbconn = self.db
+        curr = dbconn.cursor()
+        curr.execute("""DELETE FROM incidents WHERE id=%s""", [id])
+        self.db.commit()
+        return {"Message": "Incident Deleted"}
+
+    def updateIncident(self, id, title, incident, location, status, description, createdBy):
+        dbconn = self.db
+        curr = dbconn.cursor()
+
+        curr.execute("UPDATE incidents SET title=%s, incident=%s, location=%s, status=%s, description=%s, createdBy=%s WHERE id=%s",
+                     (title, incident, location, status, description, createdBy, id))
+
+        self.db.commit()
+        return {"Message": "Incident Updated"}
--- app/api/v2/views/incidents.py
+++ app/api/v2/views/incidents.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask import jsonify, make_response, request
 
-from ..models.Incidents import IncidentsModel
+from ..models.Incidents import IncidentsModel, IncidentModel
 
 
 class IncidentsView(Resource, IncidentsModel):
@@ -29,5 +29,39 @@ class IncidentsView(Resource, IncidentsModel):
                               status, description, createdBy)
         return make_response(jsonify({
             'Status': 'Ok',
-            'My Incidents': result
+            'Message': 'Incident Created'
+        }), 201)
+
+
+class IncidentView(Resource, IncidentModel):
+    def __init__(self):
+        self.db = IncidentModel()
+
+    def get(self, id):
+        res = self.db.getIncident(id)
+        return make_response(jsonify({
+            'Status': 'Ok',
+            'My Incident': res
+        }), 201)
+
+    def delete(self, id):
+        self.db.deleteIncident(id)
+        return {
+            "Message": "Incident Deleted"
+        }
+
+    def put(self, id):
+        data = request.get_json()
+        title = data['title']
+        incident = data['incident']
+        location = data['location']
+        status = data['status']
+        description = data['description']
+        createdBy = data['createdBy']
+
+        result = self.db.updateIncident(id, title, incident, location,
+                                        status, description, createdBy)
+        return make_response(jsonify({
+            'Status': 'Ok',
+            'Message': 'Incident Updated'
         }), 201)
