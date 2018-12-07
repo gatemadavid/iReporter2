7e9287b8897e60694b0ca70baac75ccce2f26bb7
Gatema David <dshefard@gmail.com>
2018-12-07 10:08:27 +0300

    add user dashboard route

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,7 +2,7 @@ from flask_restful import Api
 from flask import Blueprint
 
 from .views.users import UsersView, UserView, LoginView
-from .views.incidents import IncidentsView, IncidentView
+from .views.incidents import IncidentsView, IncidentView, UserIncidentsView
 
 version2 = Blueprint('api2', __name__, url_prefix='/api/v2')
 api = Api(version2)
@@ -11,4 +11,5 @@ api.add_resource(UsersView, '/users')
 api.add_resource(LoginView, '/login')
 api.add_resource(UserView, '/users/<int:id>')
 api.add_resource(IncidentsView, '/incidents')
+api.add_resource(UserIncidentsView, '/dashboard')
 api.add_resource(IncidentView, '/incidents/<int:id>')
