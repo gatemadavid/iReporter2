9d520e831e9adbc4379efa37ccd1ea41c290bb97
Gatema David <dshefard@gmail.com>
2018-12-10 11:02:19 +0300

    Add test for jwt token generation

--- app/tests/v2_tests/test_ireporter.py
+++ app/tests/v2_tests/test_ireporter.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 from app import create_app
-from ...api.db_config import create_tables, destroy_tables
+from ...api.db_config import test_tables, destroy_tables
 users_url = "/api/v2/users"
 user_login_url = "/api/v2/login"
 incidents_url = "/api/v2/incidents"
@@ -14,7 +14,7 @@ class RedFlagsTestCase(unittest.TestCase):
 
         self.client = self.app.test_client
 
-        create_tables()
+        test_tables()
         self.incident = {
             "title": "Corruption in Police HQ",
             "incident": "Red Flag",
@@ -24,95 +24,97 @@ class RedFlagsTestCase(unittest.TestCase):
             "createdBy": "David"
         }
         self.user = {
-            "firstname": "David",
+            "firstname": "MIKE",
             "lastname": "Muriithi",
             "email": "Muriithi@gmail.com",
-            "username": "Davido",
+            "username": "mike",
             "phone": "98890",
-            "isAdmin": "True",
-            "password": "test1234"}
+            "isAdmin": "false",
+            "password": "12333"
+        }
+        self.user2 = {
+            "firstname": "Daniel",
+            "lastname": "Gates",
+            "email": "Muriithi@gmail.com",
+            "username": "mike123",
+            "phone": "23445",
+            "isAdmin": "false",
+            "password": "12333"
+        }
+        self.user_login = {
+            "username": "mikwwedd11llm1",
+            "password": "12333"
+
+        }
 
     def register_user(self):
         res = self.client().post(users_url, data=json.dumps(
-            self.user),  headers={'content-type': "application/json"})
-        self.assertEqual(res.status_code, 201)
-
-    def login_user(self, username="Davido", password="test1234"):
-
-        user_data = {
-            'username': username,
-            'password': password
-        }
-        return self.client().post(user_login_url, data=json.dumps(
-            user_data),  headers={'content-type': "application/json"})
+            self.user), content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+        
+    def get_token(self):
+        res = self.client().post(user_login_url,
+                                 data=json.dumps(self.user_login),
+                                 headers={'content-type': "application/json"})
+        self.assertEqual(res.status_code, 200)
+        access_token = json.loads(res.data.decode)['token']
+        print(access_token)
+        return access_token
 
     def test_get_users(self):
-        self.register_user()
-        res = self.login_user()
-        access_token = json.loads(res.data.decode())['token']
+        access_token = self.get_token()
         res = self.client().get(users_url, headers=dict(
             Authorization="Bearer " + access_token))
         self.assertEqual(res.status_code, 200)
 
     def test_get_single_user(self):
-        self.register_user()
-        res = self.login_user()
-        access_token = json.loads(res.data.decode())['token']
-        self.assertEqual(res.status_code, 201)
-        result = self.client().get("/api/v2/users/1")
+        access_token = self.get_token()
+        result = self.client().get("/api/v2/users/1", headers=dict(
+            Authorization="Bearer " + access_token))
         self.assertEqual(result.status_code, 201)
 
-    # def test_delete_user(self):
-    #     res = self.client().post(users_url, data=json.dumps(
-    #         self.user), content_type='application/json')
-    #     self.assertEqual(res.status_code, 201)
-    #     result = self.client().delete("/api/v2/users/1")
-    #     self.assertEqual(result.status_code, 200)
+    def test_delete_user(self):
+        access_token = self.get_token()
+        result = self.client().delete("/api/v2/users/1", headers=dict(
+            Authorization="Bearer " + access_token))
+        self.assertEqual(result.status_code, 200)
 
-    # def test_update_user(self):
-    #     res = self.client().post(users_url, data=json.dumps(
-    #         self.user), content_type='application/json')
-    #     self.assertEqual(res.status_code, 201)
-
-    # def test_user_login(self):
-    #     pass
+    def test_update_user(self):
+        res = self.client().put(users_url, data=json.dumps(
+            self.user2), content_type='application/json')
+        self.assertEqual(res.status_code, 201)
 
     #     # test incidents
 
     def test_incident_creation(self):
-        self.register_user()
-        res = self.login_user()
-        access_token = json.loads(res.data)['token']
+        access_token = self.get_token()
         res = self.client().post(incidents_url, data=json.dumps(
             self.incident),  headers=dict(Authorization="Bearer " + access_token))
         self.assertEqual(res.status_code, 201)
 
     def test_get_incidents(self):
-        self.register_user()
-        res = self.login_user()
-        access_token = json.loads(res.data.decode())['token']
+        access_token = self.get_token()
         res = self.client().get(incidents_url, headers=dict(
             Authorization="Bearer " + access_token))
         self.assertEqual(res.status_code, 201)
 
-    # def test_get_single_incident(self):
-    #     res = self.client().post(incidents_url, data=json.dumps(
-    #         self.incident), content_type='application/json')
-    #     self.assertEqual(res.status_code, 201)
-    #     result = self.client().get("/api/v2/incidents/1")
-    #     self.assertEqual(result.status_code, 201)
+    def test_get_single_incident(self):
+        access_token = self.get_token()
+        result = self.client().get("/api/v2/incidents/1",
+                                   headers=dict(Authorization="Bearer " + access_token))
+        self.assertEqual(result.status_code, 201)
 
-    # def test_delete_incident(self):
-    #     res = self.client().post(incidents_url, data=json.dumps(
-    #         self.incident), content_type='application/json')
-    #     self.assertEqual(res.status_code, 201)
-    #     result = self.client().delete("/api/v2/incidents/1")
-    #     self.assertEqual(result.status_code, 200)
+    def test_delete_incident(self):
+        access_token = self.get_token()
+        result = self.client().delete("/api/v2/incidents/1",
+                                      headers=dict(Authorization="Bearer " + access_token))
+        self.assertEqual(result.status_code, 200)
 
-    # def test_update_incident(self):
-    #     res = self.client().post(incidents_url, data=json.dumps(
-    #         self.incident), content_type='application/json')
-    #     self.assertEqual(res.status_code, 201)
+    def test_update_incident(self):
+        access_token = self.get_token()
+        res = self.client().put(incidents_url, data=json.dumps(
+            self.incident), headers=dict(Authorization="Bearer " + access_token))
+        self.assertEqual(res.status_code, 201)
 
     def tearDown(self):
         destroy_tables()
