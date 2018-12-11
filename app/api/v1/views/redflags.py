2318e0b9a500fdf52ff07a451e4d900233e55bcd
Gatema David <dshefard@gmail.com>
2018-12-11 16:19:40 +0300

    Remove unnecessary model calls and reduce number of arguments passed to a function

--- app/api/v1/views/redflags.py
+++ app/api/v1/views/redflags.py
@@ -12,8 +12,18 @@ class RedFlags(Resource):
         title = data['title']
         description = data['description']
         location = data['location']
+        images = data['images']
+        status = data['status']
         incident_type = data['type']
-        self.db.save(title, description, location, incident_type)
+        data = {
+            "title": title,
+            "description": description,
+            "location": location,
+            "incident_type": incident_type,
+            "status": status,
+            "images": images
+        }
+        self.db.save(data)
         return make_response(jsonify({
             "message": "Red Flag Created"
         }), 201)
@@ -30,26 +40,23 @@ class RedFlag(Resource):
         self.db = RedFlagsModel()
 
     def get(self, flag_id):
-        resp = self.db.getSingleFlag(flag_id)
+        resp = self.db.get_single_flag(flag_id)
         return make_response(jsonify({
             "Message": "success",
             "Red Flags": resp
         }), 200)
 
     def delete(self, flag_id):
-        self.db.deleteFlag(flag_id)
+        self.db.delete_flag(flag_id)
         return {
             "Message": "Flag Deleted"
         }
 
     def put(self, flag_id):
         data = request.get_json()
-        title = data['title']
         description = data['description']
         location = data['location']
-        incident_type = data['type']
-        resp = self.db.updateFlag(
-            flag_id, title, description, location, incident_type)
+        resp = self.db.update_flag(flag_id, description, location)
         return make_response(jsonify({
             "Status": 200,
             "Message": "Incident updated",
