1c4e850834b6cfdbbd18b151660cde9206e328e8
Gatema David <dshefard@gmail.com>
2018-12-12 08:20:40 +0300

    add tests for delete function in users and redflags

--- app/tests/v1_tests/test_ireporter.py
+++ app/tests/v1_tests/test_ireporter.py
@@ -17,24 +17,37 @@ class RedFlagsTestCase(unittest.TestCase):
             "title": "corruption",
             "description": "Bribery",
             "location": "Ruiru",
-            "type": "red flag"}
+            "type": "red flag",
+            "status": "new",
+            "images": "images"
+        }
         self.data = {
             "id": "1",
-            "title": "corruption",
             "description": "Bribing police officers",
             "location": "Ruiru",
-            "type": "red flag"}
+            "status": "new"
+        }
         self.user_data = {
             "id": "1",
-            "fname": "David",
-            "lname": "Gatema",
-            "email": "david@gmail.com",
-            "password": "password",
+            "firstname": "David",
+            "lastname": "Muriithi",
+            "username": "mufasa21",
+            "email": "muriithi@gmail.com",
+            "password": "david123",
+            "is_admin": "false"
+        }
+        self.user2_data = {
+            "id": "1",
+            "firstname": "Mercy",
+            "lastname": "Muriithi",
+            "username": "Wangari2018",
+            "email": "wanga@gmail.com",
+            "password": "wanga1234",
             "is_admin": "true"
         }
         self.client = self.app.test_client
 
-    def test_redflag_creation(self):
+    def test_create_redflag(self):
         res = self.client().post(redflags_url, data=json.dumps(
             self.payload),  headers={'content-type': "application/json"})
         data = json.loads(res.get_data())
@@ -90,6 +103,25 @@ class RedFlagsTestCase(unittest.TestCase):
         res = self.client().post(users_url, data=json.dumps(
             self.user_data), content_type='application/json')
         self.assertEqual(res.status_code, 201)
+
+    def test_delete_user(self):
+        res = self.client().post(users_url, data=json.dumps(
+            self.user_data), content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        result = self.client().delete("/api/v1/users/1")
+        self.assertEqual(result.status_code, 200)
+        resp = json.loads(result.get_data())
+        self.assertEqual(resp['Message'], "User Deleted")
+
+    def test_edit_user(self):
+        res = self.client().post(users_url, data=json.dumps(self.user_data),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+
+        res_put = self.client().put('/api/v1/users/1',
+                                    data=json.dumps(self.user2_data),
+                                    content_type='application/json')
+        self.assertEqual(res_put.status_code, 200)
 
 
 if __name__ == "__main__":
