fa73d53063437d65ae0fc46c26dc9bfcceced63c
Gatema David <dshefard@gmail.com>
2018-12-12 08:15:50 +0300

    fix the routes for creating and getting users

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -8,4 +8,4 @@ api = Api(version1)
 api.add_resource(RedFlags, '/redflags')  # get all flags
 api.add_resource(RedFlag, '/redflags/<int:flag_id>')  # get one flag
 api.add_resource(Users, '/users')
-api.add_resource(User, '/users/<int:id>')
+api.add_resource(User, '/users/<int:user_id>')
