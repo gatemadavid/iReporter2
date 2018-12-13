d58352495b5e08882215b2533a087ebd2e21b216
Gatema David <dshefard@gmail.com>
2018-12-13 13:12:38 +0300

    create basetest that can be used by both v1 and v2 tests

---
+++ app/tests/v2_tests/base_test.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+from app import create_app
+from ...api.db_config import create_tables
+
+
+class BaseTest(TestCase):
+    def setUp(self):
+        create_app.testing = True
+
+        self.app = create_app(config_name='testing').test_client
+
+        create_tables()
+
+        self.incident = {
+            "title": "Corruption in Police",
+            "incident": "Redflag",
+            "location": "Ronald Ngala Street",
+            "description": "Potholes on the road",
+            "images": "pothole.jpg"
+        }
+        self.incident2 = {
+            "title": "Corruption in Lands ministry",
+            "incident": "intervention",
+            "location": "Ardhi house",
+            "description": "Bribing officials",
+            "images": "corrupt.jpg"
+        }
+        self.user = {
+
+            "firstname": "Alex",
+            "lastname": "Mwangi",
+            "email": "Muriithi@gmail.com",
+            "username": "muriithi",
+            "phone": "98890",
+            "isAdmin": "false",
+            "password": "1233"
+        }
+        self.user2 = {
+            "firstname": "Daniel",
+            "lastname": "Gates",
+            "email": "Muriithi@gmail.com",
+            "username": "muriithi",
+            "phone": "23445",
+            "isAdmin": "false",
+            "password": "12333"
+        }
+        self.user_login = {
+
+            "username": "muriithi",
+            "password": "1233"
+
+        }
+        
