bab1e86b3892e9ce7bf029ff832c7cd293020184
Gatema David <dshefard@gmail.com>
2018-12-14 09:19:59 +0300

    change app settings to run test db

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import create_app
-app = create_app()
+import os
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
