b8feeed25b85b009d58f0f842739d5d086265182
Gatema David <dshefard@gmail.com>
2018-12-14 09:32:13 +0300

    add test for delete user and incident

--- app/tests/v2_tests/test_ireporter.py
+++ app/tests/v2_tests/test_ireporter.py
@@ -1,10 +1,5 @@
 import json
 from .base_test import BaseTest
-from ...api.db_config import destroy_tables
-
-users_url = "/api/v2/users"
-user_login_url = "/api/v2/login"
-incidents_url = "/api/v2/incidents"
 
 
 class RedFlagsTestCase(BaseTest):
@@ -16,7 +11,16 @@ class RedFlagsTestCase(BaseTest):
         self.assertEqual(result['Message'], "User Registered. Please login")
         self.assertEqual(resp.status_code, 201)
 
+    def test_duplicate_user_register(self):
+        self.test_register_user()
+        res = self.app().post('/api/v2/users', data=json.dumps(
+            self.user), content_type='application/json')
+        resp = json.loads(res.get_data())
+        self.assertEqual(resp['Message'], "Username already exists")
+
     def test_login_user(self):
+        self.app().post('/api/v2/users', data=json.dumps(
+            self.user), content_type='application/json')
         resp = self.app().post('/api/v2/login',
                                data=json.dumps(self.user_login),
                                headers={'content-type': "application/json"})
@@ -26,7 +30,7 @@ class RedFlagsTestCase(BaseTest):
 
     def test_get_users(self):
         access_token = self.test_login_user()
-        res = self.app().get(users_url, headers=dict(
+        res = self.app().get('/api/v2/users/', headers=dict(
             Authorization="Bearer " + access_token))
         self.assertEqual(res.status_code, 200)
 
@@ -45,11 +49,13 @@ class RedFlagsTestCase(BaseTest):
 
     def test_delete_user(self):
         access_token = self.test_login_user()
+        self.app().post('/api/v2/users', data=json.dumps(
+            self.user), content_type='application/json')
         result = self.app().delete("/api/v2/users/1", headers=dict(
             Authorization="Bearer " + access_token))
         self.assertEqual(result.status_code, 200)
 
-    # # test incidents
+    # # # test incidents
 
     def test_incident_creation(self):
         access_token = self.test_login_user()
@@ -60,18 +66,20 @@ class RedFlagsTestCase(BaseTest):
 
     def test_get_incidents(self):
         access_token = self.test_login_user()
-        res = self.app().get(incidents_url, headers=dict(
+        res = self.app().get('/api/v2/incidents', headers=dict(
             Authorization="Bearer " + access_token))
         self.assertEqual(res.status_code, 201)
 
     def test_get_single_incident(self):
         access_token = self.test_login_user()
+        self.test_incident_creation()
         result = self.app().get("/api/v2/incidents/1", headers=dict(
             Authorization="Bearer " + access_token))
         self.assertEqual(result.status_code, 201)
 
     def test_update_incident(self):
         access_token = self.test_login_user()
+        self.test_incident_creation()
         res = self.app().put('/api/v2/incidents/1', data=json.dumps(
             self.incident2), content_type='application/json',
             headers=dict(Authorization="Bearer " + access_token))
@@ -79,9 +87,7 @@ class RedFlagsTestCase(BaseTest):
 
     def test_delete_incident(self):
         access_token = self.test_login_user()
+        self.test_incident_creation()
         result = self.app().delete("/api/v2/incidents/1", headers=dict(
             Authorization="Bearer " + access_token))
-        self.assertEqual(result.status_code, 200)
-
-    def tearDown(self):
-        destroy_tables()
+        self.assertEqual(result.status_code, 201)
