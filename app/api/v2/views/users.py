94c2b1fae821abb1fb7063cf6687ec3909b3684b
Gatema David <dshefard@gmail.com>
2018-12-14 09:29:44 +0300

    remove camel case in variable and method names

--- app/api/v2/views/users.py
+++ app/api/v2/views/users.py
@@ -2,14 +2,12 @@ from flask_restful import Resource
 from flask import jsonify, make_response, request, abort
 from werkzeug.security import generate_password_hash
 
-from ..models.Users import UsersModel, UserModel
+from ..models.Users import UsersModel
 
 from ..models.Incidents import IncidentsModel
-import os
-secret_key = os.getenv('SECRET')
 
 
-class UsersView(Resource, UsersModel):
+class UsersView(Resource):
     def __init__(self):
         self.db = UsersModel()
 
@@ -24,7 +22,7 @@ class UsersView(Resource, UsersModel):
         raw_pass = data['password']
         password = generate_password_hash(raw_pass)
 
-        user = self.db.registerUsers(username)
+        user = self.db.register_users(username)
         if len(user) != 0:
             return make_response(jsonify({
                 'Message': 'Username already exists'
@@ -36,12 +34,12 @@ class UsersView(Resource, UsersModel):
             return make_response(jsonify({
                 "Message": "User Registered. Please login"
             }), 201)
-    
+
     def get(self):
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
         if access_token:
-            users = self.db.getusers()
+            users = self.db.get_users()
             return make_response(jsonify({
                 "Users": users,
                 "Message": "All Users"
@@ -51,7 +49,7 @@ class UsersView(Resource, UsersModel):
             abort(404)
 
 
-class LoginView(Resource, UsersModel):
+class LoginView(Resource):
     def __init__(self):
         self.db = UsersModel()
         self.user_db = IncidentsModel()
@@ -61,19 +59,18 @@ class LoginView(Resource, UsersModel):
         username = data['username']
         password = data['password']
         auth = self.db.authenticate(username, password)
-        print(auth)
         return auth
 
 
-class UserView(Resource, UserModel):
+class UserView(Resource, ):
     def __init__(self):
-        self.db = UserModel()
+        self.db = UsersModel()
 
     def get(self, id):
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
         if access_token:
-            res = self.db.getUser(id)
+            res = self.db.get_single_user(id)
             return make_response(jsonify({
                 'Response': res
             }), 201)
@@ -82,7 +79,7 @@ class UserView(Resource, UserModel):
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
         if access_token:
-            self.db.deleteUser(id)
+            self.db.delete_user(id)
             return {
                 "Message": "User Deleted"
             }
@@ -100,8 +97,8 @@ class UserView(Resource, UserModel):
             isAdmin = data['isAdmin']
             password = data['password']
 
-            self.db.updateUser(id, firstname, lastname, email,
-                               username, phone, isAdmin, password)
+            self.db.update_user(id, firstname, lastname, email,
+                                username, phone, isAdmin, password)
             return make_response(jsonify({
                 'Status': 'Ok',
                 'Message': 'User Details Updated'
