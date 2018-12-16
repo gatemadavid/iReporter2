012796269e1f5af2473a7ee338e58cee9da063f1
Gatema David <dshefard@gmail.com>
2018-12-16 22:12:13 +0300

    refactor arguments passed to functions

--- app/api/v1/models/Users.py
+++ app/api/v1/models/Users.py
@@ -8,18 +8,13 @@ class UsersModel():
 
     def saveUser(self, data):
         user_id = len(users_db) + 1
-        fname = data['fname']
-        lname = data['lname']
-        email = data['email']
-        password = data['password']
-        isAdmin = data['isAdmin']
         user_data = {
             "id": user_id,
-            "fname": fname,
-            "lname": lname,
-            "email": email,
-            "password": password,
-            "isAdmin": isAdmin
+            "fname": data['fname'],
+            "lname":  data['lname'],
+            "email": data['email'],
+            "password": data['password'],
+            "isAdmin": data['isAdmin']
         }
         self.users.append(user_data)
         return self.users
@@ -38,16 +33,11 @@ class UsersModel():
                 return users_db.remove(user)
 
     def updateUser(self, user_id, data):
-        fname = data['fname']
-        lname = data['lname']
-        email = data['email']
-        password = data['password']
-        isAdmin = data['isAdmin']
         for user in users_db:
             if user['id'] == user_id:
-                user['fname'] = fname
-                user['lname'] = lname
-                user['email'] = email
-                user['password'] = password
-                user['isAdmin'] = isAdmin
+                user['fname'] = data['fname']
+                user['lname'] = data['lname']
+                user['email'] = data['email']
+                user['password'] = data['password']
+                user['isAdmin'] = data['isAdmin']
                 return user
