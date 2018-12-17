951b0d9e3e2afaec1419419aacec6b615bca0d38
Gatema David <dshefard@gmail.com>
2018-12-17 13:02:00 +0300

    change config file

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from instance.config import app_config
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.url_map.strict_slashes = False
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config['development'])
     create_tables()
     app.register_blueprint(v1)
     app.register_blueprint(v2)
